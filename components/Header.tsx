ce3852b12d6d7c139ff3adfa8bfd9aacf008eb3f
Alejou343 <alejandro.auribe1@gmail.com>
2023-07-31 12:12:12 -0500

    Vibrate function added

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,11 +3,16 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 const Header = () => {
+
+    const vibrateFn = (): void => {
+        window.navigator.vibrate([200, 100, 200])
+    }
+
     return (
         <div className="flex flex-col justify-center items-center h-20 mb-8 mt-14">
             <h1 className="font-bold text-3xl mb-6 flex items-center">
                 <p className="text-green-700">Avocado</p> &nbsp; 
-                <Image src="/images/headerIcon.png" alt="Avo" width={64} height={64} />
+                <Image src="/images/headerIcon.png" alt="Avo" width={64} height={64} onClick = {() => vibrateFn()}/>
                 &nbsp; <p className="text-yellow-800">Store</p>
             </h1>
             <Link href="/yes-or-no" className="text-yellow-300 font-semibold mb-8"> Should I eat an avocado today?</Link>
